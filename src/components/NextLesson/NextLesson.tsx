9c600c15b2a8152345065c0710e4b2213002c7c4
DzmitryYesis <yesis1988@mail.ru>
2022-05-06 01:01:23 +0300

    refactor: formatting styles for NextLesson component

--- src/components/NextLesson/NextLesson.tsx
+++ src/components/NextLesson/NextLesson.tsx
@@ -1,37 +1,13 @@
-import {Image, Text, View} from 'react-native';
+import {Image, StyleSheet, Text, View} from 'react-native';
 import {logoTeacher} from '../../constants/url';
 
 
 export const NextLesson = () => (
-    <View style={{
-        backgroundColor: '#d2d1d1',
-        borderRadius: 10,
-        display: 'flex',
-        flexDirection: 'row',
-        justifyContent: 'space-around',
-        width: '90%',
-        height: 120
-    }}>
-        <Image source={{uri: logoTeacher}} style={{
-            height: '80%',
-            width: 100,
-            borderRadius: 50,
-            marginTop: 10
-        }}/>
-        <View style={{
-            display: 'flex',
-            flexDirection: 'column',
-            justifyContent: 'space-around',
-            alignItems: 'flex-start',
-            width: '60%'
-        }}>
-            <View>
+    <View style={styles.wrapper}>
+        <Image source={{uri: logoTeacher}} style={styles.image}/>
+        <View style={styles.body}>
                 <Text style={{fontSize: 20, fontWeight: '500'}}>English, Elena</Text>
-            </View>
-            <View>
-                <Text style={{fontSize: 20, fontWeight: '700'}}>Tomorrow,
-                    14:00</Text>
-            </View>
+                <Text style={{fontSize: 20, fontWeight: '700'}}>Tomorrow, 14:00</Text>
         </View>
         <View>
             <Text onPress={() => {
@@ -40,3 +16,28 @@ export const NextLesson = () => (
         </View>
     </View>
 )
+
+const styles = StyleSheet.create({
+    wrapper: {
+        backgroundColor: '#d2d1d1',
+        borderRadius: 10,
+        display: 'flex',
+        flexDirection: 'row',
+        justifyContent: 'space-around',
+        width: '90%',
+        height: 120
+    },
+    image: {
+        height: '80%',
+        width: 100,
+        borderRadius: 50,
+        marginTop: 10
+    },
+    body: {
+        display: 'flex',
+        flexDirection: 'column',
+        justifyContent: 'space-around',
+        alignItems: 'flex-start',
+        width: '60%'
+    }
+})

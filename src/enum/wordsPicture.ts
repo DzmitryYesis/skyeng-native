f9c18f437f203f479071b8a7e36dd352e374336e
DzmitryYesis <yesis1988@mail.ru>
2022-05-06 01:07:46 +0300

    refactor: formatting styles for situation function

--- src/enum/wordsPicture.ts
+++ src/enum/wordsPicture.ts
@@ -3,6 +3,4 @@ export enum WORD {
     lastLesson = 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSaZVBahYHSpbwxJi9M4YoYK6CS7hD9BuCScQ&usqp=CAU',
     browser = 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQ-T6_HsXEHgremCxnlg2GwEsKeCFOfXJcAmQ&usqp=CAU',
     nextLesson = 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcT0d2kFEPJ_YgN-GyYmyUMsZn7Td7XrUcT4lw&usqp=CAU'
-
-
 }
--- src/flatListFunction/situation.tsx
+++ src/flatListFunction/situation.tsx
@@ -1,24 +1,38 @@
-import {Image, ListRenderItem, Text, TouchableOpacity, View} from 'react-native';
+import {
+    Image,
+    ListRenderItem,
+    StyleSheet,
+    Text,
+    TouchableOpacity,
+    View
+} from 'react-native';
 import {DataSituationsType} from '../state/situationsState';
 
 export const blockSituation: ListRenderItem<DataSituationsType> = ({item}) => (
     <TouchableOpacity onPress={() => {
         alert('В данный момент работа с ситуациями не возможно. Приносим свои извинения')
     }}>
-        <View style={{
-            display: 'flex',
-            flexDirection: 'column',
-            justifyContent: 'space-around',
-            alignItems: 'center',
-            width: 150,
-            height: 150,
-            marginTop: 10,
-            padding: 10
-        }}>
+        <View style={styles.wrapper}>
             <Image source={{uri: item.logo}}
-                   style={{height: '70%', width: '100%', borderRadius: 10}}/>
+                   style={styles.image}/>
             <Text>{item.title}</Text>
             <Text>{item.numberOfLessons} lessons</Text>
         </View>
     </TouchableOpacity>
 )
+
+const styles = StyleSheet.create({
+    wrapper: {
+        display: 'flex',
+        flexDirection: 'column',
+        justifyContent: 'space-around',
+        alignItems: 'center',
+        width: 150,
+        height: 150,
+        marginTop: 10,
+        padding: 10
+    },
+    image: {
+        height: '70%', width: '100%', borderRadius: 10
+    }
+})

5799e5151bef72574c9395c74f2f23f4c3ade648
DzmitryYesis <yesis1988@mail.ru>
2022-05-06 00:55:02 +0300

    refactor: formatting styles for Dictionary component

--- src/components/Dictionary/Dictionary.tsx
+++ src/components/Dictionary/Dictionary.tsx
@@ -1,27 +1,13 @@
-import {FlatList, Image, Text, TouchableOpacity, View} from 'react-native';
+import {FlatList, Image, StyleSheet, Text, TouchableOpacity, View} from 'react-native';
 import {DATA_WORDS} from '../../state/wordsState';
 import {blockWords} from '../../flatListFunction/word';
 
 export const Dictionary = () => (
     <View style={{width: '90%'}}>
-        <View style={{
-            marginTop: 25,
-            display: 'flex',
-            flexDirection: 'row',
-            justifyContent: 'space-between',
-            width: '90%',
-            height: 80
-        }}>
-            <View style={{
-                display: 'flex',
-                flexDirection: 'column',
-                justifyContent: 'space-between',
-                alignItems: 'flex-start',
-                width: '100%'
-            }}>
+        <View style={styles.wrapper}>
+            <View style={styles.head}>
                 <View>
-                    <Text style={{fontSize: 30, fontWeight: '900'}}>Изучение
-                        слов</Text>
+                    <Text style={{fontSize: 30, fontWeight: '900'}}>Изучение слов</Text>
                 </View>
                 <View>
                     <Text style={{fontSize: 20}}>0 из 4 упражнений</Text>
@@ -38,21 +24,39 @@ export const Dictionary = () => (
             </View>
         </View>
         <View style={{height: 210, width: '100%'}}>
-            <FlatList data={DATA_WORDS} renderItem={blockWords}
-                      horizontal={true}/>
+            <FlatList data={DATA_WORDS} renderItem={blockWords} horizontal={true}/>
         </View>
-        <TouchableOpacity style={{
-            backgroundColor: '#d2d1d1',
-            marginTop: 15,
-            height: 50,
-            width: '100%',
-            borderRadius: 10,
-            alignItems: 'center',
-            justifyContent: 'center'
-        }} onPress={() => {
+        <TouchableOpacity style={styles.button} onPress={() => {
             alert('Словарь находится в разработке')
         }}>
             <Text style={{fontSize: 20, fontWeight: '600'}}>Словарь</Text>
         </TouchableOpacity>
     </View>
 )
+
+const styles = StyleSheet.create({
+    wrapper: {
+        marginTop: 25,
+        display: 'flex',
+        flexDirection: 'row',
+        justifyContent: 'space-between',
+        width: '90%',
+        height: 80
+    },
+    head: {
+        display: 'flex',
+        flexDirection: 'column',
+        justifyContent: 'space-between',
+        alignItems: 'flex-start',
+        width: '100%'
+    },
+    button: {
+        backgroundColor: '#d2d1d1',
+        marginTop: 15,
+        height: 50,
+        width: '100%',
+        borderRadius: 10,
+        alignItems: 'center',
+        justifyContent: 'center'
+    }
+})

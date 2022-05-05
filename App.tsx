85e343ab71a717b41b8d56fa1216b38cef0395e1
DzmitryYesis <yesis1988@mail.ru>
2022-05-05 15:49:05 +0300

    init: add input for find words

--- App.tsx
+++ App.tsx
@@ -4,7 +4,7 @@ import {StyleSheet, Text, TextInput, View} from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <TextInput style={styles.findWords} placeholder={'Поиск слов'}/>
+      <TextInput style={styles.findWords}  placeholder={'Поиск слов'}/>
       <StatusBar style="auto" />
     </View>
   );
@@ -15,13 +15,16 @@ const styles = StyleSheet.create({
     flex: 1,
     flexDirection: 'column',
     backgroundColor: '#fff',
-    alignItems: 'flex-start',
-    justifyContent: 'center',
+    alignItems: 'center',
+    justifyContent: 'flex-start',
   },
   findWords:{
     height: 40,
+    width: '90%',
     margin: 12,
     borderWidth: 1,
+    borderRadius: 10,
+    backgroundColor: '#d2d1d1',
     padding: 10,
   }
 });

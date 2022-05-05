8c00895171b4c019907601984eddfa7094523e03
DzmitryYesis <yesis1988@mail.ru>
2022-05-05 16:31:52 +0300

    init: add block with info about lesson and teacher

--- App.tsx
+++ App.tsx
@@ -1,10 +1,26 @@
 import { StatusBar } from 'expo-status-bar';
-import {StyleSheet, Text, TextInput, View} from 'react-native';
+import {Image, StyleSheet, Text, TextInput, View} from 'react-native';
+
+const logoTeacher = 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQSk_ezlPG6XlRkE4s3mGcAov4_MnRDsvbWNg&usqp=CAU';
 
 export default function App() {
   return (
     <View style={styles.container}>
       <TextInput style={styles.findWords}  placeholder={'Поиск слов'}/>
+      <View style={{backgroundColor: '#d2d1d1', borderRadius: 10, display: 'flex', flexDirection: 'row', justifyContent: 'space-around', width: '90%', height: 120}}>
+        <Image source={{uri:logoTeacher}} style={{height: '80%', width: 100, borderRadius: 50, marginTop: 10}}/>
+        <View style={{display: 'flex', flexDirection: 'column', justifyContent: 'space-around', alignItems: 'flex-start', width: '60%'}}>
+          <View>
+            <Text style={{fontSize: 20}}>Английский, Котоморда</Text>
+          </View>
+          <View>
+            <Text style={{fontSize: 20, fontWeight: '700'}}>Завтра, 14:00</Text>
+          </View>
+        </View>
+        <View style={{marginTop: 20}}>
+          <Text style={{fontSize: 20}}>...</Text>
+        </View>
+      </View>
       <StatusBar style="auto" />
     </View>
   );
@@ -22,7 +38,6 @@ const styles = StyleSheet.create({
     height: 40,
     width: '90%',
     margin: 12,
-    borderWidth: 1,
     borderRadius: 10,
     backgroundColor: '#d2d1d1',
     padding: 10,

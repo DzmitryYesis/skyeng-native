c5c7143664b321ce4fedc2450f2b69111bfa981a
DzmitryYesis <yesis1988@mail.ru>
2022-05-06 17:36:24 +0300

    refactor: start project on Expo Go

--- src/components/Menu/Menu.tsx
+++ src/components/Menu/Menu.tsx
@@ -4,15 +4,15 @@ export const Menu = () => (
     <View style={{display: 'flex', flexDirection: 'row', margin: 10}}>
         <Button title={'Home'} onPress={() => {
             alert('Страница в разработке')
-        }}/>/
+        }}/>
         <Button title={'Homeworks'} onPress={() => {
             alert('Страница в разработке')
-        }}/>/
+        }}/>
         <Button title={'Messages'} onPress={() => {
             alert('Страница в разработке')
-        }}/>/
+        }}/>
         <Button title={'Profile'} onPress={() => {
             alert('Страница в разработке')
-        }}/>/
+        }}/>
     </View>
 )
